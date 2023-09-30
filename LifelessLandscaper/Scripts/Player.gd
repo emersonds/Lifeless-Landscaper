47a58efe5f8086e9a2a0611820e1561708e65e6a
Dylan Emerson <emersonds2868@gmail.com>
2023-09-30 10:25:19 -0400

    Started Mower Behavior with temp object
    - Created a temporary object "ExampleMesh" as child of the player to start work on mower behavior.
    - The object follows the player while a child, and is offset around the player depending on where the player is moving. If the player is moving left, the mower is on the left side of the player, etc.
    - When the player stops mowing by pressing 'Space', the temp object is removed as a child from the player and added to the root node so the player can drop it.
    - The player cannot currently pick the example mower back up yet.

--- LifelessLandscaper/Scripts/Player.gd
+++ LifelessLandscaper/Scripts/Player.gd
@@ -1,11 +1,41 @@
 extends CharacterBody2D
 
 @export var move_speed: float = 50.0
+@export var mow_speed_multiplier: float = 0.75
+@export var mower_offset: float = 12.0
 
+var is_mowing: bool = true
+
+@onready var mower_object = $ExampleMesh
+
+# Called every frame
+func _process(delta):
+	CheckMowing()
+
+# Called every second
 func _physics_process(delta):
-	velocity = Vector2(Input.get_axis("move_left", "move_right"),
+	# Get movement vector
+	var dir = Vector2(Input.get_axis("move_left", "move_right"),
 			Input.get_axis("move_up", "move_down")).normalized()
 	
-	velocity *= move_speed
+	# Multiply by speed
+	velocity = dir * move_speed
 	
+	# Change speed if mowing
+	if is_mowing:
+		velocity *= mow_speed_multiplier
+		if is_instance_valid(mower_object):
+			mower_object.position = dir * mower_offset
+	
+	# Move player
 	move_and_slide()
+
+# Check if player starts mowing.
+func CheckMowing():
+	if Input.is_action_just_pressed("toggle_mow"):
+		is_mowing = not is_mowing
+	
+	if is_mowing == false:
+		if is_instance_valid(mower_object):
+			remove_child(mower_object)
+			get_node("/root").add_child(mower_object)
